224c6057e708cfb262e61da91d4c128dc26e9042
Martijn van Halen <mvanhalen@outlook.com>
2019-07-02 15:42:22 +0200

    Issue sample

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Loadable from 'svelte-loadable'
+</script>
+
+<h1>What now?</h1>
+<Loadable  loader={() => import('https://igtemplate.blob.core.windows.net/public/packages/svelte/ScreenCDN.svelte')}>
+  <div slot="loading">Loading...</div>
+  <div slot="error" let:error>
+    {error}
+  </div>
+</Loadable>
+<mystuff/>	
